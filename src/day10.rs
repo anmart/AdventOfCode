d5181d568a64b4b186646bc7fada490019827cf3
Andrew Martinek <andrewrmartinek@gmail.com>
2021-12-10 00:30:48 -0500

    day 10 done :)

---
+++ src/day10.rs
@@ -0,0 +1,55 @@
+#[allow(dead_code)]
+pub fn part1(lines: String) -> i32{
+	let mut tot = 0;
+	for line in lines.lines(){
+		let mut parse = vec!();
+		for a in line.chars(){
+			match a {
+				'['|'('|'<'|'{' => parse.push(a),
+				'}' => if parse.pop().unwrap() != '{' {tot += 1197},
+				']' => if parse.pop().unwrap() != '[' {tot += 57},
+				')' => if parse.pop().unwrap() != '(' {tot += 3},
+				'>' => if parse.pop().unwrap() != '<' {tot += 25137},
+				_ => println!("???"),
+			}
+		}
+	}
+	return tot;
+}
+
+#[allow(dead_code)]
+pub fn part2(lines: String) -> i32{
+	let mut woog = vec!();
+	'outer: for line in lines.lines(){
+		let mut parse = vec!();
+		for a in line.chars(){
+			match a {
+				'['|'('|'<'|'{' => parse.push(a),
+				'}' => if parse.pop().unwrap() != '{' {continue 'outer},
+				']' => if parse.pop().unwrap() != '[' {continue 'outer},
+				')' => if parse.pop().unwrap() != '(' {continue 'outer},
+				'>' => if parse.pop().unwrap() != '<' {continue 'outer},
+				_ => println!("???"),
+			}
+
+		}
+		
+		let mut score: u64 = 0;
+		for c in parse.iter().rev(){
+			score *= 5;
+			score += match c{
+				'(' => 1,
+				'[' => 2,
+				'{' => 3,
+				'<' => 4,
+				_ => 0,
+			}
+		}
+		woog.push(score);
+	}
+	let z = woog.len() as f32 / 2.0;
+	woog.sort();
+	println!("{:?}", woog[z as usize]);
+	return woog[z as usize] as i32;
+}
+
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day10;
 mod helper;
 
 use std::time::Instant;
@@ -16,12 +17,12 @@ fn main() {
 	let _before_io = Instant::now();
 
 	// read data
-	let data = read_file_to_string("./data/day9").unwrap();
+	let data = read_file_to_string("./data/day10").unwrap();
 
 	// get current time for benchmarking
 	let _before_run = Instant::now();
 
-	println!("{}", day9::part2(data));
+	println!("{}", day10::part2(data));
 
 	// print benchmark results
 	println!("Elapsed time: {:?}", _before_run.elapsed());
